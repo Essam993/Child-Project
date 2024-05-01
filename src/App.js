cba5a73be25835c812fab2659e774a49daf65a63
Ahmed Essam <Ahmed.essam@tenten-pay.com>
2024-05-01 13:30:16 +0300

    child revamp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import './styles.css'
 
 function App() {
   return (
-    <>
-      <h1>Child Project</h1>
-    </>
+    <article>
+      <h2>How to stay on top of things:</h2>
+      <ol>
+        <li><del>Make a list</del></li>
+        <li>Track your time</li>
+        <li>Time box all tasks</li>
+        <li>Do more difficult tasks first</li>
+        <li>Take breaks!!</li>
+        <li><u>FOCUS</u>: Don't multitask</li>
+        <li><del>z-index: 4;</del></li>
+        <li><del>z-index: 9999;</del></li>
+        <li><del>z-index: 999999999;</del></li>
+        <li>z-index: max(infinity);</li>
+      </ol>
+    </article>
   );
 }
 
